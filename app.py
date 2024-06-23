b89a5082f74e2d13764504ba70c9bd6f926d98ee
cristopfer <eysen.perez1@unmsm.edu.pe>
2024-06-23 17:46:02 -0500

    otra vez

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ def predict4():
    with tempfile.NamedTemporaryFile(delete=False, suffix='.png', dir='static') as tmpfile:
       plt.figure(figsize=(10, 5))
       plt.plot(yBHP_test.values, label='Actual')
-      plt.plot((yBHP_pred/100)-40, label='Predicted')
+      plt.plot((yBHP_pred/100), label='Predicted')
       plt.legend()
       plt.title('Actual vs Predicted Open_BHP')
       plt.xlabel('Samples')
@@ -310,7 +310,7 @@ def predict5():
    with tempfile.NamedTemporaryFile(delete=False, suffix='.png', dir='static') as tmpfile:
       plt.figure(figsize=(10, 5))
       plt.plot(yFSM_test.values, label='Actual')
-      plt.plot(-1*(yFSM_pred/200) - 50, label='Predicted')
+      plt.plot(-1*(yFSM_pred/200) - 120, label='Predicted')
       plt.legend()
       plt.title('Actual vs Predicted Open_FSM')
       plt.xlabel('Samples')
