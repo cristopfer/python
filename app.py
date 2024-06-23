5c34678894c4efb75fe79f5ad69bd6bda296977c
cristopfer <eysen.perez1@unmsm.edu.pe>
2024-06-23 18:48:11 -0500

    otra vez

--- app.py
+++ app.py
@@ -477,11 +477,18 @@ def predict8():
 def predict9():
    fecha_seleccionada = request.form['fecha']
    fecha_futura = datetime.strptime(fecha_seleccionada, '%Y-%m-%d')
-   df2 = pd.read_csv(file_path)
-   featuresBVN2 = ['High_BVN', 'Low_BVN', 'Adj Close_BVN','Open_GLD','High_GLD', 'Low_GLD', 'Adj Close_GLD', 'Open_GCF', 'High_GCF','Low_GCF', 'Adj Close_GCF', 'Open_GSPC', 'High_GSPC','Low_GSPC', 'Close_GSPC', 'Open_PEN_X', 'High_PEN_X', 'Low_PEN_X', 'Close_PEN_X','Indicadores de riesgo - Perú', 'Year_df']
+   fecha_seleccionada = pd.to_datetime(fecha_seleccionada)
+   df = pd.read_csv(file_path)
+   df['Year_df'] = pd.to_datetime(df['Year_df'])
+   df2 = df[df['Year_df'] < fecha_seleccionada]
+   featuresBVN2 = ['High_BVN', 'Low_BVN', 'Adj Close_BVN','Open_GLD','High_GLD', 'Low_GLD', 'Adj Close_GLD', 'Open_GCF', 'High_GCF','Low_GCF', 'Adj Close_GCF', 'Open_GSPC', 'High_GSPC','Low_GSPC', 'Close_GSPC', 'Open_PEN_X', 'High_PEN_X', 'Low_PEN_X', 'Close_PEN_X']
    targetBVN2 = 'Open_BVN'
    X1 = df2[featuresBVN2].iloc[1:]
-   y1 = df2[targetBVN2].iloc[1:]
+   Series_Temporal = df2[targetBVN2].shift(-1)
+   y1 = Series_Temporal.iloc[1:]
+   mask = ~y1.isna()
+   X1 = X1[mask]
+   y1 = y1[mask]
    XBVN_train, XBVN_test, yBVN_train, yBVN_test = train_test_split(X1, y1, test_size=0.2, random_state=42)
 
    scaler = StandardScaler()
@@ -490,30 +497,47 @@ def predict9():
 
    knn_model = KNeighborsRegressor(n_neighbors=5)
    knn_model.fit(XBVN_train_scaled, yBVN_train) 
+   yBVN_pred = knn_model.predict(XBVN_test_scaled)
+
+   with tempfile.NamedTemporaryFile(delete=False, suffix='.png', dir='static') as tmpfile:
+      plt.figure(figsize=(10, 5))
+      plt.plot(yBVN_test.values, label='Actual')
+      plt.plot(yBVN_pred, label='Predicted')
+      plt.legend()
+      plt.title('Actual vs Predicted Open_BVN')
+      plt.xlabel('Samples')
+      plt.ylabel('Open_BVN')
+      plt.savefig(tmpfile.name, format='png')
+      tmpfile_path = tmpfile.name
+   
+   image_url = url_for('static', filename=os.path.basename(tmpfile_path))
    
    ultima_fila = df2[featuresBVN2].iloc[1]
    nueva_entrada = ultima_fila.copy()
-   nueva_entrada['Year_df'] = fecha_futura.year
-   print("Nueva entrada antes del escalado:", nueva_entrada)
    X_new = pd.DataFrame([nueva_entrada])
    X_new_scaled = scaler.transform(X_new)
-   print("Nueva entrada después del escalado:", X_new_scaled)
 
    prediccion = knn_model.predict(X_new_scaled)
-   return jsonify({'fecha': fecha_seleccionada, 'prediccion': prediccion[0]})
+   return jsonify({'prediccion': prediccion[0], 'imagen': image_url})
 
 @app.route('/predict10', methods=['POST'])
 def predict10():
    fecha_seleccionada = request.form['fecha']
    fecha_futura = datetime.strptime(fecha_seleccionada, '%Y-%m-%d')
-   df2 = pd.read_csv(file_path)
+   fecha_seleccionada = pd.to_datetime(fecha_seleccionada)
+   df = pd.read_csv(file_path)
+   df['Year_df'] = pd.to_datetime(df['Year_df'])
+   df2 = df[df['Year_df'] < fecha_seleccionada]
    featuresBHP2 = ['High_T03_BHP', 'Low_T03_BHP', 'Adj Close_T03_BHP','Open_SIF','High_SIF', 'Low_SIF',
    'Adj Close_SIF', 'Open_HGF', 'High_HGF', 'Low_HGF', 'Adj Close_HGF','Open_GSPC', 'High_GSPC',
-   'Low_GSPC', 'Close_GSPC', 'Open_DJI', 'High_DJI', 'Low_DJI',
-   'Close_DJI','Year_df']
+   'Low_GSPC', 'Close_GSPC', 'Open_DJI', 'High_DJI', 'Low_DJI','Close_DJI']
    targetBHP2 = 'Open_T03_BHP'
    X1 = df2[featuresBHP2].iloc[1:]
-   y1 = df2[targetBHP2].iloc[1:]
+   Series_Temporal = df2[targetBHP2].shift(-1)
+   y1 = Series_Temporal.iloc[1:]
+   mask = ~y1.isna()
+   X1 = X1[mask]
+   y1 = y1[mask]
    XBHP_train, XBHP_test, yBHP_train, yBHP_test = train_test_split(X1, y1, test_size=0.2, random_state=42)
 
    scaler = StandardScaler()
@@ -522,30 +546,48 @@ def predict10():
 
    knn_model = KNeighborsRegressor(n_neighbors=5)
    knn_model.fit(XBHP_train_scaled, yBHP_train) 
+   yBHP_pred = knn_model.predict(XBHP_test_scaled)
+
+   with tempfile.NamedTemporaryFile(delete=False, suffix='.png', dir='static') as tmpfile:
+      plt.figure(figsize=(10, 5))
+      plt.plot(yBHP_test.values, label='Actual')
+      plt.plot(yBHP_pred, label='Predicted')
+      plt.legend()
+      plt.title('Actual vs Predicted Open_BHP')
+      plt.xlabel('Samples')
+      plt.ylabel('Open_BHP')
+      plt.savefig(tmpfile.name, format='png')
+      tmpfile_path = tmpfile.name
    
+   image_url = url_for('static', filename=os.path.basename(tmpfile_path))
+
    ultima_fila = df2[featuresBHP2].iloc[1]
    nueva_entrada = ultima_fila.copy()
-   nueva_entrada['Year_df'] = fecha_futura.year
-   print("Nueva entrada antes del escalado:", nueva_entrada)
    X_new = pd.DataFrame([nueva_entrada])
    X_new_scaled = scaler.transform(X_new)
-   print("Nueva entrada después del escalado:", X_new_scaled)
 
    prediccion = knn_model.predict(X_new_scaled)
-   return jsonify({'fecha': fecha_seleccionada, 'prediccion': prediccion[0]})
+   return jsonify({'prediccion': prediccion[0], 'imagen': image_url})
 
 @app.route('/predict11', methods=['POST'])
 def predict11():
    fecha_seleccionada = request.form['fecha']
    fecha_futura = datetime.strptime(fecha_seleccionada, '%Y-%m-%d')
-   df2 = pd.read_csv(file_path)
+   fecha_seleccionada = pd.to_datetime(fecha_seleccionada)
+   df = pd.read_csv(file_path)
+   df['Year_df'] = pd.to_datetime(df['Year_df'])
+   df2 = df[df['Year_df'] < fecha_seleccionada]
    featuresFSM2 = ['High_T05_SCCO', 'Low_T05_SCCO', 'Adj Close_T05_SCCO','Open_SIF','High_SIF', 'Low_SIF',
    'Adj Close_SIF', 'Open_HGF', 'High_HGF', 'Low_HGF', 'Adj Close_HGF','Open_GSPC', 'High_GSPC',
    'Low_GSPC', 'Close_GSPC', 'Open_DJI', 'High_DJI', 'Low_DJI',
    'Close_DJI','Year_df']
    targetFSM2 = 'Open_T05_SCCO'
    X1 = df2[featuresFSM2].iloc[1:]
-   y1 = df2[targetFSM2].iloc[1:]
+   Series_Temporal = df2[targetFSM2].shift(-1)
+   y1 = Series_Temporal.iloc[1:]
+   mask = ~y1.isna()
+   X1 = X1[mask]
+   y1 = y1[mask]
    XFSM_train, XFSM_test, yFSM_train, yFSM_test = train_test_split(X1, y1, test_size=0.2, random_state=42)
 
    scaler = StandardScaler()
@@ -554,15 +596,28 @@ def predict11():
 
    knn_model = KNeighborsRegressor(n_neighbors=5)
    knn_model.fit(XFSM_train_scaled, yFSM_train) 
+   yFSM_pred = knn_model.predict(XFSM_test_scaled)
+
+   with tempfile.NamedTemporaryFile(delete=False, suffix='.png', dir='static') as tmpfile:
+      plt.figure(figsize=(10, 5))
+      plt.plot(yFSM_test.values, label='Actual')
+      plt.plot(yFSM_pred, label='Predicted')
+      plt.legend()
+      plt.title('Actual vs Predicted Open_FSM')
+      plt.xlabel('Samples')
+      plt.ylabel('Open_FSM')
+      plt.savefig(tmpfile.name, format='png')
+      tmpfile_path = tmpfile.name
    
+   image_url = url_for('static', filename=os.path.basename(tmpfile_path))
+
    ultima_fila = df2[featuresFSM2].iloc[1]
    nueva_entrada = ultima_fila.copy()
-   nueva_entrada['Year_df'] = fecha_futura.year
    X_new = pd.DataFrame([nueva_entrada])
    X_new_scaled = scaler.transform(X_new)
 
    prediccion = knn_model.predict(X_new_scaled)
-   return jsonify({'fecha': fecha_seleccionada, 'prediccion': prediccion[0]})
+   return jsonify({'prediccion': prediccion[0], 'imagen': image_url})
 
 if __name__ == '__main__':
    port = int(os.environ.get('PORT', 5000))
