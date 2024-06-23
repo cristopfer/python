2ce87571eed4172a75a3517b04c49cdf4e4c0060
cristopfer <eysen.perez1@unmsm.edu.pe>
2024-06-23 04:14:32 -0500

    subido

--- app.py
+++ app.py
@@ -72,8 +72,6 @@ def predict():
    print(fecha_seleccionada)
    fecha_futura = datetime.strptime(fecha_seleccionada, '%Y-%m-%d')
    df2 = pd.read_csv(file_path)
-   fecha_seleccionada = pd.to_datetime(fecha_seleccionada)
-   print(fecha_seleccionada)
    df2 = df2[df2.index < fecha_seleccionada]
    featuresBVN1 = ['High_BVN', 'Low_BVN', 'Adj Close_BVN','Open_GLD',
        'High_GLD', 'Low_GLD', 'Adj Close_GLD', 'Open_GCF', 'High_GCF',
